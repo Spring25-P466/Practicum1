b4577ba58f34af7e6bf30c8f1f72c30869598f44
HiC-BGOD <Blane3512@gmail.com>
2025-02-18 18:16:21 -0500

    2nd initial setup

--- src/main/java/edu/iu/p466_anderblm/prime_service/controller/PrimesController.java
+++ src/main/java/edu/iu/p466_anderblm/prime_service/controller/PrimesController.java
@@ -1,4 +1,24 @@
 package edu.iu.p466_anderblm.prime_service.controller;
 
+import edu.iu.p466_anderblm.prime_service.service.IPrimeService;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@CrossOrigin
+@RequestMapping("/primes")
 public class PrimesController {
+    IPrimeService primeService;
+
+    public PrimesController(IPrimeService primeService)
+    {
+        this.primeService = primeService;
+    }
+
+
+    @GetMapping("/{n}")
+    public boolean isPrime(@PathVariable int n) {
+        return primeService.isPrime(n);
+    }
+
 }
---
+++ src/main/java/edu/iu/p466_anderblm/prime_service/service/IPrimeService.java
@@ -0,0 +1,5 @@
+package edu.iu.p466_anderblm.prime_service.service;
+
+public interface IPrimeService {
+    boolean isPrime(long n);
+}
